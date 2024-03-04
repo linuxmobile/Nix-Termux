c0a3a2a3682541b788011a1f7af3f84c9604ad2b
Zhang Hua <zhanghua.00@qq.com>
2024-03-04 14:42:00 +0800

    Add corrected ssh configuration

--- nix-on-droid/home-manager/programs/ssh.nix
+++ nix-on-droid/home-manager/programs/ssh.nix
@@ -1,4 +1,12 @@
 {
     enable = true;
-    compress = true;
+    compression = true;
+    matchBlocks = {
+        "github.com" = {
+            user = "git";
+            identityFile = [
+                "~/.ssh/id_ed25519.github.com"
+            ];
+        };
+    };
 }
