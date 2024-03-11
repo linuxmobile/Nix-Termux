2f05044697b56e23d7470fc4cf454089172e84ee
Zhang Hua <zhanghua.00@qq.com>
2024-03-11 08:20:10 +0800

    Switch Nix-on-Droid to release-23.11

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
     
     nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/prerelease-23.11";
+      url = "github:nix-community/nix-on-droid/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
